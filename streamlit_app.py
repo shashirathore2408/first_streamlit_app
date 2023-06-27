6ee9f84c455a3ca094208148f70b5118c6789ee0
shashirathore2408 <135882178+shashirathore2408@users.noreply.github.com>
2023-06-27 11:30:35 +0530

    Update streamlit_app.py
    adding streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import pandas
+import streamlit
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
